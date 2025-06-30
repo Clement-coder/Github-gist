2df4b54fc29c89d6a308116731d7da472db5a2fd
Clement Raymond <chinexzy37@gmail.com>
2025-06-30 10:09:50 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let example = [3,6,9,4,5,2]
+let example = [3,2,2,4,5,2]
 let output = {}
 let number = example[0]
-
 example.forEach(num => {
     output[num] = (output[num] || 0) + 1;
     if(output[num] > output[number]) {
         number = num
     }
 })
-
 console.log("Number", number)
 console.log("occurence", output[number])
 
