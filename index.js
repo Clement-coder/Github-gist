2d5f5cfcc159b5bc312e2ea7623709019c3f3e66
Clement Raymond <chinexzy37@gmail.com>
2025-06-30 09:54:56 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let example = [1,6,2,4,5,2]
+let output = {}
+let number = example[0]
+
+example.forEach(num => {
+    output[num] = (output[num] || 0) + 1;
+    if(output[num] > output[number]) {
+        number = num
+    }
+});
+
+console.log("Number", number)
+console.log("occurence", output[number])
+
